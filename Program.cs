4b0f5972f9899d58137ebc9aef490699a19ffd6b
2 Anh Uông Hoàng <1821051033@student.humg.edu.vn>
2021-01-17 10:41:20 +0700

    add new project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PhuongTrinhBacNhat{
+class program
+    {
+static void main(string[] args){
+    Console.Write("Nhap he so a:");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write("Nhap he so b:");
+    int b = int.Parse(Console.ReadLine());
+            if (a == 0)
+                if (b == 0)
+                    Console.WriteLine("Phuong trinh co vo so nghiem");
+                else
+                    Console.WriteLine("Phuong trinh vo so nghiem");
+            else{
+                int ketqua = -b / a;
+                Console.WriteLine("Phuong trinh co nghiem la x={0}");
+                ketqua.ToString();
+            }
+            Console.ReadLine();
+        }
+    }
+} 
